21725cb262a94b8472ab2e078aa1c4617af3962d
Hafid-stack <abdelhafid.idbahamd@gmail.com>
2025-08-14 00:20:34 +0100

    Displaying Products

--- src/app/product/product.ts
+++ src/app/product/product.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-product',
@@ -7,6 +7,23 @@ import { Component } from '@angular/core';
   styleUrl: './product.css',
   standalone: true
 })
-export class Product {
+export class Product implements OnInit {
+  products!: Array<any>;
+  constructor() {
+  }
+  ngOnInit() {
+    this.products = [
+      {id:1,name:"Computer",price:100,selected:true},
+      {id:2,name:"Printer",price:856,selected:false},
+      {id:3,name:"Iphone",price:9000,selected:true}
+    ]
+  }
 
+  handleDelete(product: any) {
+    let v=confirm("Are you sure you want to delete this product?");
+    if (v) {
+      this.products=this.products.filter(p => p.id != product.id )
+    }
+
+  }
 }
