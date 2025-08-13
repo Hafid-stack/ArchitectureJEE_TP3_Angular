46db24588c160c79ca6c2b93002065999960670a
Hafid-stack <abdelhafid.idbahamd@gmail.com>
2025-08-13 23:22:12 +0100

    Creating components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {Product} from './product/product';
+import {Home} from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:"home" , component:Home},
+    {path:"product", component:Product},
+]
+;
